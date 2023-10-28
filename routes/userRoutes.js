faf28acdfad0306957702a71a6de8dc0e5a7d5b9
emilianomarotta <emilianomarott@gmail.com>
2023-10-28 01:19:02 -0300

    feat(authController): add routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 router.post('/register', userController.registerUser);
+router.post('/login', authController.authenticateUser);
+
 
 module.exports = router;
