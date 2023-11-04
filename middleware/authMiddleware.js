2355347bc78c2c60a7d1b8d1100ca08ab97210f2
emilianomarotta <emilianomarott@gmail.com>
2023-11-04 01:02:53 -0300

    fix(authMiddleware): check if token is not invalid, fix header Authorization

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,10 +1,20 @@
+const fs = require('fs');
+const path = require('path');
 const jwt = require('jsonwebtoken');
+const invalidTokensFilePath = path.join(__dirname, '../data/invalidTokens.txt');
+
 const secretKey = 'mySecretKey';
+
 const authMiddleware = (req, res, next) => {
-  const token = req.header('x-auth-token');
+  const token = req.headers.authorization;
 
   if (!token) {
     return res.status(401).json({ message: 'No se proporcionó un token de autenticación.' });
+  }
+
+  const invalidTokens = getInvalidTokens();
+  if (invalidTokens.includes(token)) {
+    return res.status(401).json({ message: 'Token ya no es válido.' });
   }
 
   try {
@@ -15,5 +25,10 @@ const authMiddleware = (req, res, next) => {
     return res.status(400).json({ message: 'Token inválido.' });
   }
 };
+
+function getInvalidTokens() {
+  const fileContent = fs.readFileSync(invalidTokensFilePath, 'utf-8');
+  return JSON.parse(fileContent);
+}
 
 module.exports = authMiddleware;
