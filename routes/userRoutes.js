f1fa6cfa0bf84423ac3dcbb96ce88ff6ada8a0ce
emilianomarotta <emilianomarott@gmail.com>
2023-11-03 22:03:49 -0300

    feat(favoriteController): add user favorite routes in userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const authMiddleware = require('../middleware/authMiddleware');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const favoriteController = require('../controllers/favoritesController');
 
 router.post('/register', userController.registerUser);
 router.post('/login', authController.authenticateUser);
-
+router.post('/addFavorite', authMiddleware, favoriteController.addFavoriteMovie);
 
 module.exports = router;
